80c3b02e5da75969684610bd84ec97c8209cdad1
Yaroslav Molchanov <y.molchanoff@gmail.com>
2025-04-28 23:18:49 +0300

    some message

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,9 +5,7 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (
-    compareFunction = (elem1, elem2) => {
-      return Number(elem1 > elem2);
-    },
+    compareFunction = (a, b) => (String(a) > String(b) ? 1 : -1),
   ) {
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = i + 1; j < this.length; j++) {
